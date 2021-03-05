571169dcd82c39ca71372ade86ea2311aa8de9b2
Roman <roman@ga-games.com>
2021-03-06 00:25:24 +0300

    fix

--- backend/libs/services/docker.js
+++ backend/libs/services/docker.js
@@ -18,10 +18,6 @@ module.exports.getInfo = async (docker) => {
     return await docker.info()
 }
 
-module.exports = async (docker) => {
-    console.log(await docker.listContainers().length)
-}
-
 module.exports.getContainers = async (docker) => {
     return await docker.listContainers()
 }
