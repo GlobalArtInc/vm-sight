943d9659c574bbb37df01e2d87d0d50531df9d03
Roman Khabarov <roman@ga-games.com>
2021-03-13 12:32:39 +0300

    fix

--- client/src/store/modules/app.js
+++ client/src/store/modules/app.js
@@ -1,16 +1,23 @@
 import {fetchEndpoint} from "@/api/endpoints/api";
 
 const state = {
+    loaded: false,
     endpoint: false
 }
 
 const mutations = {
+    SET_LOADED: (state, data) => {
+      state.loaded = data
+    },
     SET_ENDPOINT: (state, data) => {
         state.endpoint = data
     }
 }
 
 const actions = {
+    setLoaded({commit}) {
+        commit('SET_LOADED', true)
+    },
     getEndpoint({commit}, id) {
         return new Promise((resolve, reject) => {
             fetchEndpoint(id).then((data) => {
