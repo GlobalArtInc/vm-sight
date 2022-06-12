38b78ce77f2a2b97d3efdf0c1b144e459ffd9829
Roman Khabarov <roman@ga-games.com>
2022-06-12 17:49:23 +0300

    #1 fix build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,6 @@
-mv ../packages/sight-backend/dist
+#mv ../packages/sight-backend/dist
+cp ./packages/sight-backend/.env ./dist
+cp ./packages/sight-backend/.env.development ./dist
+cp ./packages/sight-backend/.env.production ./dist
+cp -r ./packages/sight-backend/src/migrations ./dist
+cp -r ./packages/sight-backend/node_modules ./dist
