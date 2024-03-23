3ebef6d58f09adf6bfe81cc631a9c8b7c00e7f8e
Roman Khabarov <roman@globalart.dev>
2024-03-23 06:35:55 +0000

    nuxt

---
+++ app/apps/web/nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  devServer: {
+    host: "0.0.0.0",
+    port: 80,
+  }
+})
--- app/apps/web/src/declares/index.d.ts
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue';
-
-declare module 'vue/types/vue' {
-  interface Vue {
-    t(locale: string): string;
-  }
-}
