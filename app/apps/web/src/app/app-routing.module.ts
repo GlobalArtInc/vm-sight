23359b49e235781f6c6824425de438353d6ab215
Roman Khabarov <roman@globalart.dev>
2024-03-24 17:09:34 +0000

    fix

--- app/apps/web/src/app/app-routing.module.ts
+++ app/apps/web/src/app/app-routing.module.ts
@@ -4,10 +4,15 @@ import { AuthGuard } from './modules/auth/services/auth.guard';
 
 export const routes: Routes = [
   {
+    path: 'auth',
+    loadChildren: () =>
+      import('./modules/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
     path: '',
     canActivate: [AuthGuard],
     loadChildren: () =>
-      import('./modules/layout/layout.module').then(m => m.LayoutModule),
+      import('./modules/layouts/layout.module').then(m => m.LayoutModule),
   },
   { path: '**', redirectTo: 'error/404' },
 ];
--- app/apps/web/src/app/modules/auth/services/auth.guard.ts
+++ app/apps/web/src/app/modules/auth/services/auth.guard.ts
@@ -3,17 +3,11 @@ import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Injectable({ providedIn: 'root' })
-export class AuthGuard {
+export class AuthGuard  {
   constructor(private authService: AuthService) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    // const currentUser = this.authService.currentUserValue;
-    // if (currentUser) {
-    //   // logged in so return true
-    //   return true;
-    // }
-    // not logged in so redirect to login page with the return url
-    this.authService.logout();
-    return false;
+    // todo... check if user is logged in
+    return true;
   }
 }
