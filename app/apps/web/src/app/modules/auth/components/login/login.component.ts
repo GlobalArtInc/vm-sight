03f1d8f28faeb3646ad22de497ba9d4082bdea0a
Roman Khabarov <roman@globalart.dev>
2024-03-24 17:59:14 +0000

    fix

--- app/apps/web/src/app/modules/auth/components/login/login.component.ts
+++ app/apps/web/src/app/modules/auth/components/login/login.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SharedModule } from '../../../shared/shared.module';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [SharedModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.loginForm = this.formBuilder.group({
+      username: [null, [Validators.required]],
+      password: [null, Validators.required]
+    });
+   }
+
+  submit() {
+    if (!this.loginForm.valid) {
+      return;
+    }
+    console.log(this.loginForm.value);
+  }
 
 }
