3279b4aff0331b3ce70b75e592bd0ef8c1247b56
Roman Khabarov <roman@globalart.dev>
2024-03-25 12:28:01 +0000

    fix

---
+++ app/apps/web/src/app/pages/routing.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+
+const Routing: Routes = [
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'error/404',
+  },
+];
+
+export { Routing };
